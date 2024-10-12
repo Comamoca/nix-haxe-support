f97e9e68dce278004ab7156d1968aa9acb4d6786
Comamoca <comamoca.dev@gmail.com>
2024-10-12 22:58:10 +0900

    :seedling: (flake.nix, flake.lock) Init!

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "nixpkgs haxe support piece.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    inputs@{
+      self,
+      systems,
+      nixpkgs,
+      treefmt-nix,
+      flake-parts,
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ treefmt-nix.flakeModule ];
+      systems = import inputs.systems;
+
+      perSystem =
+        {
+          config,
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          stdenv = pkgs.stdenv;
+	  haxe-lib = pkgs.callPackage { inherit pkgs; };
+        in
+        rec {
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixfmt.enable = true;
+            };
+
+            settings.formatter = { };
+          };
+
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              nil
+            ];
+          };
+
+	  haxe-support = haxe-lib;
+        };
+    };
+}
